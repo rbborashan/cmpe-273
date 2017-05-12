6a69069e5dd0e3337374c807e4ecadc90489d2d3
Ryan Borashan <rbborashan@gmail.com>
2017-05-12 14:16:29 -0700

    Add POST method for order

---
+++ assignment2/order/POST/service.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import boto3
+
+def handler(event, context):
+
+        menu_table = boto3.resource('dynamodb', region_name='us-west-2').Table('Menu')
+        order_table = boto3.resource('dynamodb', region_name='us-west-2').Table('Order')
+
+        order_table.put_item(
+            Item = {
+                'order_id': event['order_id'],
+                'menu_id': event['menu_id'],
+                'customer_name': event['customer_name'],
+                'customer_email': event['customer_email'],
+                'order': {
+                    'selection': 'none',
+                    'size': 'none',
+                    'costs': 'none',
+                    'order_time': 'none',
+                }
+            }
+        )
+
+        items = menu_table.get_item(Key={'menu_id': event['menu_id']}).get('Item').get('selection')
+        selections = dict(enumerate(items, 1))
+
+        choices=''.join('{}. {} '.format(key, val) for key, val in selections.items())
+
+        message = {
+            'Message': 'Hi ' + event['customer_name'] +
+            ', please choose one of these selections:  ' + choices
+        }
+
+        return {
+            'statusCode': '200',
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'body': message
+        }
