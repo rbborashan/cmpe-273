38ccb98006f01bdc9596371d47e0f801836ed6b7
Ryan Borashan <rbborashan@gmail.com>
2017-05-11 22:31:16 -0700

    Fix incorrect json syntax

--- assignment2/menu/DELETE/service.py
+++ assignment2/menu/DELETE/service.py
@@ -5,10 +5,13 @@ def handler(event, context):
 
     	table = boto3.resource('dynamodb', region_name='us-west-2').Table('Menu')
 
+        key = {}
+        key['menu_id'] = event['menu_id']
+
         return {
             'statusCode': '200',
             'headers': {
                 'Content-Type': 'application/json'
             },
-            'body': table.delete_item(Key = event['key'])
+            'body': table.delete_item(Key = key)
         }
